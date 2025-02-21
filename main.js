17bed2b321af8a6cc014b026619bf1cc39c2537b
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-02-21 15:52:21 +0100

    add comments

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* creo un array di bici da corsa */
+
 let bici = [
     { 
         name :"mtb",
@@ -31,15 +33,16 @@ let bici = [
     price: 300
 }
 ]
-
+/* stampo in console l'array di bici */
 console.log(bici);
-
+/* dichiaro il peso minimo */
 let pesoMinimo = 0;
-
+/* creo un ciclo per cercare la bici con il peso minore */
 for(let i = 0; i < bici.length; i++){
     if(bici[i].weight < bici[pesoMinimo].weight){
         pesoMinimo = i
     }
 }
+/* stampo in console la bici con il peso minore */
 console.log(bici[pesoMinimo].weight);
 
